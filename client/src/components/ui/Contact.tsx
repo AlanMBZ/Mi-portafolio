bb74e2af06ba424d1faed3782fee509b3daff5fc
AlanMBZ <alan200470@hotmail.com>
2026-08-25 16:12:52 -0600

    fix: actualizar dominio de produccion y arreglar CORS

--- client/src/components/ui/Contact.tsx
+++ client/src/components/ui/Contact.tsx
@@ -26,7 +26,7 @@ export default function Contact() {
 
     try {
       // Hacemos la petición al backend que acabamos de crear
-      const response = await fetch("https://mi-portafolio-m1fbh6x9a-alanmbzs-projects.vercel.app/api/contact", {
+      const response = await fetch('https://mi-portafolio-dusky-theta.vercel.app/api/contact', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
